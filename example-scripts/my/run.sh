87fee1871b632f409ea190046acb892ecb48f5ea
Johannes Heger <gldog@gmx.de>
2023-08-27 13:38:09 +0200

    Restructe example-scripts

--- example-scripts/my/run.sh
+++ example-scripts/my/run.sh
@@ -2,8 +2,10 @@
 
 REPOS_DIR="/Users/jo/prj-test/merge_git_repos/repos"
 LOGS_DIR="/Users/jo/prj-test/merge_git_repos/logs/$(date +'%Y%m%d-%H%M%S')"
+
 rm -rf "$REPOS_DIR"
 
+#The "bash -c" is needed in Git-Bash, and can be omitted otherwise.
 python3 ../../src/merge_git_repos.py \
   --repos-data \
   mb:origin/master:test-feature-branch:jheger/stash-mybranches \
@@ -12,8 +14,8 @@ python3 ../../src/merge_git_repos.py \
   --repos-dir $REPOS_DIR \
   --logs-dir $LOGS_DIR \
   --log-level DEBUG \
-  --pre-script ../../pre-merge-script-examples/my/clone_repos_and_install_merge-drivers.sh \
-  --post-script "echo 'FINISHED' ; printenv | grep MGR_ ; git -C \$MGR_REPO_DIR status" \
+  --pre-script 'bash -c ../../example-scripts/my/pre/clone_repos_and_install_merge-drivers.sh' \
+  --post-script 'bash -c "echo 'FINISHED' ; printenv | grep MGR_ ; git -C $MGR_REPO_DIR status"' \
   --merge-branch-template \
   "maintain/dsm_{{source_branch.replace('origin/','')}}_into_{{dest_branch}}_{{task_start.strftime('%b%d')}}"
 
